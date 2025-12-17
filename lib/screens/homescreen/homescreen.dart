8bcf9a9b0e2ae2c7964d55edbfac8fd67819b9a1
haileamare <haileamare1994@gmail.com>
2025-12-16 19:50:17 -0800

    homescreen ui finished

--- lib/screens/homescreen/homescreen.dart
+++ lib/screens/homescreen/homescreen.dart
@@ -19,8 +19,8 @@ class HomeScreen extends StatelessWidget {
     var width = MediaQuery.of(context).size.width;
     var height = MediaQuery.of(context).size.height;
     return Container(
-      width: width * 0.88,
-      height: height >= 812 ? 0.35 * height : height * 0.35,
+      width: width * 0.85,
+      height: height * 0.35,
       padding: EdgeInsets.all(height * 0.02),
       decoration: BoxDecoration(
         color: Colors.black.withValues(alpha: 0.5),
@@ -45,44 +45,44 @@ class HomeScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Image.asset("images/planet.png", fit: BoxFit.cover),
-              SizedBox(width: 25),
+              SizedBox(width: width*0.04),
               SingleChildScrollView(
-                scrollDirection:Axis.vertical,
-                child: Column(
-                  children: [
+                scrollDirection: Axis.vertical,
+                child:  Column(
+                    children: [
+                      SizedBox(
+                        width: width * 0.4,
                   
-                    SizedBox(
-                      width: 200,
-                
-                      child: Text(
-                        "Mars",
-                        softWrap: true,
-                        overflow: TextOverflow.visible,
-                        style: TextStyle(
-                          color: Colors.lightBlueAccent,
-                          fontWeight: FontWeight.w900,
-                          fontFamily: 'fantasy',
-                          fontSize: 16,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    SizedBox(
-                      width: 190,
-                      height: 130,
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.vertical,
                         child: Text(
-                          style: TextStyle(
-                            overflow: TextOverflow.visible,
-                            color: Colors.white,
-                          ),
+                          "Mars",
                           softWrap: true,
-                          "Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, only being larger than Mercury. In the English language, Mars is named for the Roman god of war.Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, only being larger than Mercury. In the English language, Mars is named for the Roman god of war.",
+                          overflow: TextOverflow.visible,
+                          style: TextStyle(
+                            color: Colors.lightBlueAccent,
+                            fontWeight: FontWeight.w900,
+                            fontFamily: 'fantasy',
+                            fontSize: 16,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                      SizedBox(height: height*0.002),
+                      SizedBox(
+                        width: width * 0.4,
+                        height:height<812? height *0.15:height*0.2,
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.vertical,
+                          child: Text(
+                            style: TextStyle(
+                              overflow: TextOverflow.visible,
+                              color: Colors.white,
+                            ),
+                            softWrap: true,
+                            "Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, only being larger than Mercury. In the English language, Mars is named for the Roman god of war.Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, only being larger than Mercury. In the English language, Mars is named for the Roman god of war.",
+                          ),
+                        ),
+                      ),
+                    ],
+                  
                 ),
               ),
             ],
@@ -157,16 +157,47 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
       child: Scaffold(
-        // bottomNavigationBar:BottomNavigationBar(
-        //   items:[
-        //     BottomNavigationBarItem(
-        //       icon:IconButton(
-        //         onPressed:(){},
-        //         icon:Icon(Icons.web)
-        //       ),
-        //     ),
-        //   ]
-        // ),
+        bottomNavigationBar:Container(
+          margin: EdgeInsets.symmetric(horizontal: 8),
+          decoration:BoxDecoration(
+          color:Color.fromARGB(25, 0,0,0),
+
+          ),
+          child: BottomNavigationBar(
+            backgroundColor:Colors.transparent,
+            selectedItemColor:Colors.lightBlueAccent,
+            unselectedItemColor:Colors.white60,
+            items:[
+              BottomNavigationBarItem(
+                label: "Home",
+                icon:IconButton(
+                  onPressed:(){},
+                  icon:Icon(Icons.web,color: Colors.white,)
+                ),
+                
+              ),
+              BottomNavigationBarItem(
+                label: "Favorites",
+                icon:IconButton(
+                  onPressed:(){},
+                  icon:Icon(Icons.favorite,color:Colors.white),
+                
+                ),
+                
+              ),
+               BottomNavigationBarItem(
+                label: "More",
+                
+                icon:IconButton(
+                  onPressed:(){},
+                  icon:Icon(Icons.more,color:Colors.white),
+                
+                ),
+                
+              ),
+            ]
+          ),
+        ),
         drawer: Drawer(child: Text('My drawer')),
         backgroundColor: Colors.transparent,
 
@@ -205,8 +236,8 @@ class HomeScreen extends StatelessWidget {
               Builder(
                 builder: (context) {
                   return Container(
-                    width: 46,
-                    height: 46,
+                     width: 49,
+                     height: 49,
                     decoration: BoxDecoration(
                       shape: BoxShape.circle,
                       color: Colors.transparent,
@@ -237,8 +268,8 @@ class HomeScreen extends StatelessWidget {
               ),
 
               Container(
-                width: 46,
-                height: 46,
+                width: 49,
+                height: 49,
                 decoration: BoxDecoration(
                   color: Colors.transparent,
                   shape: BoxShape.circle,
@@ -264,108 +295,141 @@ class HomeScreen extends StatelessWidget {
             0,
             0,
           ), // ✅ full-screen body background (only visible below AppBar)
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: SizedBox(
-                  height: 60,
-                  width: MediaQuery.of(context).size.width,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: planets.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 8),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(25)),
-                          color: Color.fromARGB(100, 0, 0, 0),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withValues(alpha: 0.7),
-                              blurRadius: 6,
-                              spreadRadius: 2,
-                              offset: const Offset(3, 3),
-                            ),
-                          ],
-                        ),
-                        child: SizedBox(
-                          width: 130,
-                          height: 60,
-                          child: Row(
-                            children: [
-                              Image.asset(
-                                "images/planet.png",
-                                fit: BoxFit.cover,
-                              ),
-                              Text(
-                                planets[index],
-                                style: TextStyle(color: Colors.white),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: SizedBox(
+                    height: 60,
+                    width: MediaQuery.of(context).size.width,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: planets.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(25)),
+                            color: Color.fromARGB(100, 0, 0, 0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withValues(alpha: 0.7),
+                                blurRadius: 6,
+                                spreadRadius: 2,
+                                offset: const Offset(3, 3),
                               ),
                             ],
                           ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: height * 0.38,
-                child: Expanded(
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: pdetails.length,
-                    itemBuilder: (context, index) {
-                      return Row(
-                        children: [
-                          Container(
-                            margin: const EdgeInsets.symmetric(
-                              horizontal: 8,
-                              vertical: 8,
+                          child: SizedBox(
+                            width: 130,
+                            height: 60,
+                            child: Row(
+                              children: [
+                                Image.asset(
+                                  "images/planet.png",
+                                  fit: BoxFit.cover,
+                                ),
+                                Text(
+                                  planets[index],
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ],
                             ),
-                            child: card(pdetails[index], context),
                           ),
-                        ],
-                      );
-                    },
+                        );
+                      },
+                    ),
                   ),
                 ),
-              ),
-              // Container(
-              //   width: width,
-              //   height: height * 0.38,
-              //   padding:EdgeInsets.symmetric(horizontal: 20,vertical:15),
-              //   margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
-              //   decoration: BoxDecoration(
-              //     color: Colors.black.withValues(alpha: 0.5),
-              //     borderRadius: BorderRadius.all(Radius.circular(16)),
-              //   ),
-              //   child:SingleChildScrollView(
-              //     child: Column(
-              //       crossAxisAlignment:CrossAxisAlignment.start,
-              //       children:[
-              //         Text(
-              //           "Solar System",
-              //           style:TextStyle(
-              //             color:Colors.white,
-              //             fontWeight:FontWeight.bold,
-              //             fontSize:18
-              //           ),
-              //         ),
-              //         Text("The Solar System is the gravitationally bound system of the Sun and the objects that orbit it. It formed 4.6 billion years ago from the gravitational collapse of a giant interstellar molecular cloud. The vast majority (99.86%) of the system's mass is in the Sun, with most of the remaining mass contained in the planet Jupiter. The four inner system planets—Mercury, Venus, Earth and Mars—are terrestrial planets, being composed primarily of rock and metal. The four giant planets of the outer system are substantially larger and more massive than the terrestrials. ",
-              //         style:TextStyle(
-              //           color:Colors.white70,
-              //           fontWeight:FontWeight.normal,
-              //           fontFamily:'new times roman',
-              //           fontSize:14,
-              //           wordSpacing:2
-              //         ))
-              //       ]
-              //     ),
-              //   ),
-              // ),
-            ],
+                SizedBox(
+                  height: height * 0.38,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: pdetails.length,
+                      itemBuilder: (context, index) {
+                        return Row(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.symmetric(
+                                horizontal: 8,
+                                vertical: 0,
+                              ),
+                              child: card(pdetails[index], context),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                ),
+                Container(
+                  width: width,
+                  height: height * 0.38,
+                  padding:EdgeInsets.symmetric(horizontal: 20,vertical:15),
+                  margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+                  decoration: BoxDecoration(
+                    color: Colors.black.withValues(alpha: 0.5),
+                    borderRadius: BorderRadius.all(Radius.circular(16)),
+                  ),
+                  child:SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment:CrossAxisAlignment.start,
+                      children:[
+                        Text(
+                          "Solar System",
+                          style:TextStyle(
+                            color:Colors.white,
+                            fontWeight:FontWeight.bold,
+                            fontSize:18
+                          ),
+                        ),
+                        Text("The Solar System is the gravitationally bound system of the Sun and the objects that orbit it. It formed 4.6 billion years ago from the gravitational collapse of a giant interstellar molecular cloud. The vast majority (99.86%) of the system's mass is in the Sun, with most of the remaining mass contained in the planet Jupiter. The four inner system planets—Mercury, Venus, Earth and Mars—are terrestrial planets, being composed primarily of rock and metal. The four giant planets of the outer system are substantially larger and more massive than the terrestrials. ",
+                        style:TextStyle(
+                          color:Colors.white70,
+                          fontWeight:FontWeight.normal,
+                          fontFamily:'new times roman',
+                          fontSize:14,
+                          wordSpacing:2
+                        ))
+                      ]
+                    ),
+                  ),
+                ),
+                  Container(
+                  width: width,
+                  height: height * 0.38,
+                  padding:EdgeInsets.symmetric(horizontal: 20,vertical:15),
+                  margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+                  decoration: BoxDecoration(
+                    color: Colors.black.withValues(alpha: 0.5),
+                    borderRadius: BorderRadius.all(Radius.circular(16)),
+                  ),
+                  child:SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment:CrossAxisAlignment.start,
+                      children:[
+                        Text(
+                          "Solar System",
+                          style:TextStyle(
+                            color:Colors.white,
+                            fontWeight:FontWeight.bold,
+                            fontSize:18
+                          ),
+                        ),
+                        Text("The Solar System is the gravitationally bound system of the Sun and the objects that orbit it. It formed 4.6 billion years ago from the gravitational collapse of a giant interstellar molecular cloud. The vast majority (99.86%) of the system's mass is in the Sun, with most of the remaining mass contained in the planet Jupiter. The four inner system planets—Mercury, Venus, Earth and Mars—are terrestrial planets, being composed primarily of rock and metal. The four giant planets of the outer system are substantially larger and more massive than the terrestrials. ",
+                        style:TextStyle(
+                          color:Colors.white70,
+                          fontWeight:FontWeight.normal,
+                          fontFamily:'new times roman',
+                          fontSize:14,
+                          wordSpacing:2
+                        ))
+                      ]
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
